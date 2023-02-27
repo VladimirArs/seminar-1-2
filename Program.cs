2c0a84f0fee2155914f0471742183cfc3ab81b19
VladimirArs <125826542+VladimirArs@users.noreply.github.com>
2023-02-28 00:00:00 +0300

    ДЗ
    Добавлено 4 файла

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int Prompt (string message)
+{
+    Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int inValue = Prompt("Веедите целое число");
+int number = 2;
+
+while (number <= inValue)
+{
+    Console.WriteLine(number);
+    number += 2;
+}
